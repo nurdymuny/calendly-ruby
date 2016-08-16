aab1c08ab3630f80c8884bd2593221574211d351
nurdymuny <brian@nurdymuny.com>
2016-08-17 02:11:37 +0700

    refactored Calendly module method

--- lib/calendly.rb
+++ lib/calendly.rb
@@ -1,6 +1,7 @@
 require "calendly/version"
 require "calendly/configuration"
 require "calendly/error_msg"
+require "net/http"
 require "json"
 
 module Calendly
@@ -21,36 +22,18 @@ module Calendly
   def self.test_authentication
     checking_token
     
-    uri = URI("#{CALENDLY_URL}echo")
-
-		http = Net::HTTP.new(uri.host, uri.port)
-		http.use_ssl = true
-		http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-		
-		request = Net::HTTP::Get.new(uri.request_uri)
-    request["X-TOKEN"] = @configuration.token
-
-		response = http.request(request)
-
+    url = "#{CALENDLY_URL}echo"
+   
+    response = request(url, 'get')
 		data = response.body
 		response.code.eql?("204") ? {message: 'Authentication valid!'} : JSON.parse(data)
   end
  
   def self.webhook_subscription(params={})
   	checking_token
-    uri = URI("#{CALENDLY_URL}hooks")
-    # Create the HTTP objects
-		http = Net::HTTP.new(uri.host, uri.port)
-		http.use_ssl = true
-		http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    url = "#{CALENDLY_URL}hooks"
 
-		request = Net::HTTP::Post.new(uri.request_uri)
-    request["X-TOKEN"] = @configuration.token
-		request.content_type = "application/json"
-
-		request.body = params.to_json
-		# Send the request
-		res = http.request(request)
+		res = request(url, 'post', params)
 
     data = res.body
     JSON.parse(data)
@@ -58,19 +41,9 @@ module Calendly
 
   def self.delete_webhook(hook_id)
     checking_token
-    #12400
-    uri = URI("#{CALENDLY_URL}hooks/#{hook_id}")
-    # Create the HTTP objects
-		http = Net::HTTP.new(uri.host, uri.port)
-		http.use_ssl = true
-		http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    url = "#{CALENDLY_URL}hooks/#{hook_id}"
 
-		request = Net::HTTP::Delete.new(uri.request_uri)
-    request["X-TOKEN"] = @configuration.token
-		request.content_type = "application/json"
-
-		# Send the request
-		res = http.request(request)
+		res = request(url, 'delete')
 
     data = res.body
     res.code.eql?('200') ? {message: 'Success'} : JSON.parse(data)
@@ -81,26 +54,33 @@ module Calendly
 
     url_text = canceled ? "#{CALENDLY_URL}invitees/samples" : "#{CALENDLY_URL}invitees/samples?canceled=true"
 
-    uri = URI(url_text)
-
-		http = Net::HTTP.new(uri.host, uri.port)
-		http.use_ssl = true
-		http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-		
-		request = Net::HTTP::Get.new(uri.request_uri)
-    request["X-TOKEN"] = @configuration.token
-
-		response = http.request(request)
+		response = request(url_text, 'get')
 
 		data = response.body
     JSON.parse(data)
   end
 
-  def self.checking_token
-    raise Calendly::ErrorMsg.new(msg: "Authentication Token have not yet setup.", error: {status: 202, message: 'Authentication Token have not yet setup'}) unless configured? 
+  def self.request(url, req_type, params={})
+    uri = URI(url)
+
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      
+    request = eval "Net::HTTP::#{req_type.capitalize}.new(uri.request_uri)"
+    request["X-TOKEN"] = @configuration.token
+    request.content_type = "application/json"
+    request.body = params.to_json if params.any?
+
+    http.request(request)
   end
 
   def self.configured?
     @configuration && !@configuration.token.nil?
+  end
+
+  private
+  def self.checking_token
+    raise Calendly::ErrorMsg.new(msg: "Authentication Token have not yet setup.", error: {status: 202, message: 'Authentication Token have not yet setup'}) unless configured? 
   end
 end
